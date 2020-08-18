7c1453d0b179a75efed15eecbae6550a55385f35
samthegitguy <55744004+samthegitguy@users.noreply.github.com>
2020-08-18 13:04:53 +1000

    Create progress bar

---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Incognito mode initializing.");
+        System.out.print("Progress: [#");
+        for(int i=0;i<100;i++){ 
+        System.out.print("#");
+        Thread.sleep(50);
+        }
+        System.out.print("#]");
+
+
+    }
+}
