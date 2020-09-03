255e18810b8863ce635776a367002f7fc6cc2baf
samthegitguy <55744004+samthegitguy@users.noreply.github.com>
2020-09-04 09:17:25 +1000

    .

--- src/Emulator.java
+++ src/Emulator.java
@@ -1,27 +1,26 @@
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+
 /** @author samthegitguy
  * @version pre-alpha */
 
 // Main Class 
 public class Emulator
 { 
-	static String reader(String readType) throws IOException
-	{
+	static String reader(final String readType) throws IOException {
 		System.out.println("What is your " + readType + "?");
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
-		String output = reader.readLine();
-		System.out.println("Your " + readType + " is: " + output);
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		final String output = reader.readLine();
 		return output;
 	}
-	public static void main(final String[] args) throws InterruptedException, IOException
-	{ 
+
+	public static void main(final String[] args) throws InterruptedException, IOException {
 		/**
 		 * Displays progress bar while the child thread runs the actual program.
 		 *
 		 */
-		Runnable r = new MyRunnable(reader("username"), reader("email"), reader("password"));
+		final Runnable r = new MyRunnable(reader("username"), reader("email"), reader("password"));
 		new Thread(r).start();
         System.out.println("Incognito mode initializing.");
         System.out.print("Progress: [#");
