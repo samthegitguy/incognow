d5a13bd2eaff4d4abc81faf45fe7cf7c2afbed5d
samthegitguy <55744004+samthegitguy@users.noreply.github.com>
2020-09-03 14:08:10 +1000

    Collect email name and password (no function linked)

--- src/Emulator.java
+++ src/Emulator.java
@@ -1,32 +1,50 @@
-class SecondaryThread extends Thread 
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/** @author samthegitguy
+ * @since
+ * @version pre-alpha
+ * 
+ * Secondary thread for actual background worker */
+class SecondaryThread extends Thread
 { 
 	public void run() 
 	{ 
-		/**
-		 * Runs
-		 */
+		System.out.println("Background worker running");
 	} 
 } 
 
 // Main Class 
 public class Emulator
 { 
-	public static void main(String[] args) throws InterruptedException
+	static String reader(String readType) throws IOException
+	{
+		System.out.println("What is your " + readType + "?");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in)); 
+		String output = reader.readLine();
+		System.out.println("Your " + readType + " is: " + output);
+		return output;
+	}
+	public static void main(final String[] args) throws InterruptedException, IOException
 	{ 
 		/**
-		 * Displays progress bar. Due to thread.sleep this class throws an Interrupted exception.
+		 * Displays progress bar while the child thread runs the actual program.
 		 */
-		int n = 3; // Number of threads 
+		reader("username");
+		reader("email");
+		reader("password");
+		final int n = 1; // Number of threads 
 		for (int i=0; i<n; i++) 
-		{ 
-			SecondaryThread object = new SecondaryThread(); 
+		{ /** Starts n amount of secondary threads*/
+			final SecondaryThread object = new SecondaryThread(); 
 			object.start(); 
         } 
         System.out.println("Incognito mode initializing.");
         System.out.print("Progress: [#");
-        for(int i=0;i<100;i++){ 
-        System.out.print("#");
-        Thread.sleep(50);
+		for(int i=0;i<100;i++)
+		{ 
+        	System.out.print("#");
+        	Thread.sleep(50);
         }
         System.out.print("#]");
 	} 
