fdf9643a4a7be90df1b283ccae5f835e1f3621ba
samthegitguy <55744004+samthegitguy@users.noreply.github.com>
2020-09-15 10:46:48 +1000

    Remove jaunt log

--- src/SecurityCheck.java
+++ src/SecurityCheck.java
@@ -2,7 +2,7 @@ import com.jaunt.*;
 public class SecurityCheck {
     public void Checker() throws JauntException {
         UserAgent userAgent = new UserAgent();         //create new userAgent (headless browser)
-        System.out.println("SETTINGS:\n" + userAgent.settings);      //print the userAgent's default settings.
+        //print the userAgent's default settings.
         userAgent.settings.autoSaveJSON = true;        //change settings to autosave last visited page.
     
         userAgent.sendGET("https://samthegitguy.github.io/password-manager-one/securitystatus.json");   //send request
